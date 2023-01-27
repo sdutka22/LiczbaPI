6b0e2f01e4160fffb1f8389ee7c95ac5fbf9047e
Udigital <119742288+sdutka22@users.noreply.github.com>
2023-01-27 12:58:30 +0100

    added new project where we are looking for Pi number

---
+++ src/main/java/pl/sdutka/wszib/pi/App.java
@@ -0,0 +1,35 @@
+package pl.sdutka.wszib.pi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static int totalShots = 0;
+    public static int circleHits = 0;
+    public static void main(String[] args) {
+        double pi;
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            threads.add(new Thread(new ThreadsPi()));
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        try {
+            for (Thread thread : threads) {
+                thread.join();
+            }
+        } catch (InterruptedException e){
+            System.out.println("Thread interrupted: " + e.getMessage());    
+        }
+
+        pi = 4.0 * ((double) circleHits / (double) totalShots);
+
+        String output = "Ilosc strzałów: " + totalShots + "\n" +
+                "Ilosc trafień: " + circleHits + "\n" +
+                "Wyliczone Pi: " + pi + "\n";
+        System.out.println(output);
+    }
+}
---
+++ src/main/java/pl/sdutka/wszib/pi/ThreadsPi.java
@@ -0,0 +1,24 @@
+package pl.sdutka.wszib.pi;
+import java.util.Random;
+
+public class ThreadsPi implements Runnable {
+    Random rand = new Random();
+    @Override
+    public void run() {
+        int totalShots = 100000;
+        int hitsInsideCircle = 0;
+        double r = 1;
+
+        for(int i = 0; i < totalShots; i++){
+            double x = 2 * rand.nextDouble() - 1;
+            double y = 2 * rand.nextDouble() - 1;
+
+            if (Math.sqrt((x * x) + (y * y)) <= r) {
+                hitsInsideCircle++;
+            }
+        }
+        App.totalShots += totalShots;
+        App.circleHits += hitsInsideCircle;
+
+    }
+}
